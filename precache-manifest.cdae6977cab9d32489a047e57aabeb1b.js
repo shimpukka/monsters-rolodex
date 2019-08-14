1d0313e5e90d88ba7354494196944914516c74d0
Shiori Sasaki <shiori.sasaki@viidakko.fi>
2019-08-14 16:22:26 +0300

    Updates

---
+++ precache-manifest.cdae6977cab9d32489a047e57aabeb1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2bab13f28ce1830f74b82c4255abde",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4f641a41d1475aa1f9c6",
+    "url": "/monsters-rolodex/static/css/main.60ad7e64.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "4f641a41d1475aa1f9c6",
+    "url": "/monsters-rolodex/static/js/main.95e29504.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
